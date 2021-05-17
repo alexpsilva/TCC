59813440964b6e158a16b3863e34b6dbf8f5d9d3
alexpsilva <alex.paivasilva@poli.ufrj.br>
2021-05-17 09:05:28 -0300

    Minor text changes

--- cli.py
+++ cli.py
@@ -3,7 +3,7 @@ from src.github.deploy import deploy_to_github
 from src.jekyll.populate import populate_jekyll
 
 if len(sys.argv) != 4:
-    print('Invalid parameters. Please run "python3 cli.py <yml_path> <github_username> <github_token>"')
+    print('Invalid parameters. Please run "python3 cli.py <yml_path> <github_user> <github_token>"')
 
 file_path = sys.argv[1]
 user = sys.argv[2]
