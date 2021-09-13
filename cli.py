a43e4c4f913164af70f4ddc5c17bcc28f57cb381
Alexandre Silva <alex.paivasilva@poli.ufrj.br>
2021-09-12 22:58:53 -0300

    ref: Added argparse to manage CLI parameters

--- cli.py
+++ cli.py
@@ -1,15 +1,14 @@
-import sys
+import argparse
 from src.github.deploy import deploy_to_github
 from src.jekyll.populate import populate_jekyll
 
-if len(sys.argv) != 4:
-    print('Invalid parameters. Please run "python3 cli.py <yml_path> <github_user> <github_token>"')
-
-file_path = sys.argv[1]
-user = sys.argv[2]
-token = sys.argv[3]
+parser = argparse.ArgumentParser()
+parser.add_argument('yml_path', help='Absolute or relative path to the .yml file containing the process description')
+parser.add_argument('github_user', help='The Github user that shall host the generated site')
+parser.add_argument('github_token', help='The authentication token to the previously defined Github user')
+args = parser.parse_args()
 
 project_path = './_temp_jekyl_project'
 
-populate_jekyll(file_path, project_path)
-deploy_to_github(project_path, user, token)
+populate_jekyll(args.yml_path, project_path)
+deploy_to_github(project_path, args.github_user, args.github_token)
